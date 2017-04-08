5423d5143d466539db624df2d5cd5305ed119316
Jake Hobbs <jakehobbs@gmail.com>
2017-04-08 03:26:52 -0700

    many changes

--- static/js/event_list.js
+++ static/js/event_list.js
@@ -4,39 +4,27 @@ function listEvents(events) {
     return;
   }
 
-  var frag = document.createDocumentFragment();
   for (var i = 0; i < events.length; i++) {
-    // This is going to be ugly, but I'm just going to concat
-    // everything into one string and shove it into a div. Someone
-    // else should make this nicer.
     var event = events[i];
-    var text = event.event_name + ' ' + event.event_type + ' ' +
-        event.event_date + ': ';
+    var attendeeString = '';
     for (var j = 0; j < event.attendees.length; j++) {
-      text += event.attendees[j];
+      attendeeString += event.attendees[j];
       if (j !== event.attendees.length - 1) {
-        text += ', ';
+        attendeeString +=', ';
       }
     }
-    var div = document.createElement('div');
-    $(div).text(text + ' ');
 
     // Now, create the link.
     var eventLink = document.createElement('a');
     $(eventLink).text('edit');
     $(eventLink).attr('href', '/update_event/' + event.event_id);
 
-    // Now put the link inside the div.
-    $(div).append(eventLink);
+    // output to new row in table to display
+    var newRow = '<tr><td>' + event.event_date + '</td><td><b>' + event.event_name + '</b></td><td>' + event.event_type + '</td><td>' + attendeeString + '<a class="edit-link" href="' + eventLink + '"> edit</a></td></tr>';
+    var d = document.getElementById('event-list');
+    d.insertAdjacentHTML('beforeend', newRow);
 
-    // Finally, append the div to the fragment.
-    $(frag).append(div);
   }
-
-  // At this point, we have a bunch of data. Replace the contents of
-  // event-list with our new data.
-  $('#event-list').html(''); // clear list
-  $('#event-list').append(frag);
 
 }
 
