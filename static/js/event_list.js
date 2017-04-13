2d48e36ee1ca973085506cf3da05e7eba3c84a0a
Samer Masterson <samer@samertm.com>
2017-04-12 19:55:04 -0700

    refactor css

--- static/js/event_list.js
+++ static/js/event_list.js
@@ -10,7 +10,7 @@ function listEvents(events) {
     for (var j = 0; j < event.attendees.length; j++) {
       attendeeString += event.attendees[j];
       if (j !== event.attendees.length - 1) {
-        attendeeString +=', ';
+        attendeeString +='<br/>';
       }
     }
 
@@ -23,17 +23,14 @@ function listEvents(events) {
     var newRow = '<tr>' +
         '<td><a class="edit-link" href="' + eventLink + '"><span class="glyphicon glyphicon-pencil"></span></a></td>' +
         '<td nowrap>' + event.event_date + '</td>' +
-        '<td nowrap><b>' + event.event_name + '</b></td>' +
+        '<td><b>' + event.event_name + '</b></td>' +
         '<td nowrap>' + event.event_type + '</td>' +
         '<td nowrap>' + event.attendees.length + '</td>' +
         '<td>' + attendeeString + '</td>' +
         '</tr>';
     var d = document.getElementById('event-list-body');
     d.insertAdjacentHTML('beforeend', newRow);
-    console.log()
-
   }
-
 }
 
 function initializeApp() {
