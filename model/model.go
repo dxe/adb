09d90f76469c76c33f11a6848ecd57c19cb66349
Jake Hobbs <jakehobbs@gmail.com>
2017-05-14 20:19:15 -0700

    working on power hist

--- model/model.go
+++ model/model.go
@@ -748,12 +748,13 @@ FROM (
 	SELECT
 		activist_id,
 		MAX(CASE WHEN event_type = "protest" or event_type = "key event" THEN "1" ELSE "0" END) AS is_protest,
-	    MAX(CASE WHEN event_type = "outreach" or event_type = "sanctuary" or event_type = "community" THEN "1" ELSE "0" END) AS is_community
+	    MAX(CASE WHEN event_type = "outreach" or event_type = "sanctuary" or event_type = "community" THEN "1" ELSE "0" END) AS is_community,
+        SUBSTR(e.date,1,4) AS year,
+        SUBSTR(e.date,6,2) AS month
 	FROM event_attendance ea
 	JOIN events e ON ea.event_id = e.id
-	WHERE e.date BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) AND NOW()
-	GROUP BY activist_id
-	HAVING is_protest = "1" AND is_community = "1"
+	GROUP BY activist_id, year, month
+	HAVING is_protest = "1" AND is_community = "1" AND month = "` + month + `" AND year = "` + year + `"
 ) AS power_index
 `
 	var power int
