960946778a8570841d2fd8a182b78ac1abeb5ac8
Samer Masterson <nosefrog@gmail.com>
2017-06-16 21:50:08 -0700

    Build activist list page with Vue (#14)
    Start using Vue to generate the activist list page.
    
    TODO:
    
    Sort doesn't work correctly for dates + status

---
+++ frontend/ActivistList.vue
@@ -0,0 +1,163 @@
+<template>
+
+  <div id="app" class="main">
+    <table id="activist-list" class="adb-table table table-hover table-striped tablesorter">
+      <thead>
+        <tr>
+          <th></th>
+          <th @click="sortBy('name')">Name</th>
+          <th @click="sortBy('email')">Email</th>
+          <th @click="sortBy('phone')">Phone</th>
+          <th @click="sortBy('first_event')">First Event</th>
+          <th @click="sortBy('last_event')">Last Event</th>
+          <th @click="sortBy('status')">Status</th>
+        </tr>
+      </thead>
+      <tbody id="activist-list-body">
+        <tr v-for="activist in activists">
+          <td><button class="btn btn-default glyphicon glyphicon-pencil" @click="showModal(activist)"></button></td>
+          <td>{{activist.name}}</td>
+          <td>{{activist.email}}</td>
+          <td>{{activist.phone}}</td>
+          <td>{{activist.first_event}}</td>
+          <td>{{activist.last_event}}</td>
+          <td>{{activist.status}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <modal
+       name="edit-activist-modal"
+       :height="800"
+       classes="no-background-color"
+       @opened="modalOpened"
+       @closed="modalClosed"
+       >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h2 class="modal-title">Edit activist</h2>
+          </div>
+          <div class="modal-body">
+            <p><label for="name">Name: </label><input class="form-control" type="text" :value="currentActivist.name" id="name" /></p>
+            <p><label for="email">Email: </label><input class="form-control" type="text" :value="currentActivist.email" id="email" /></p>
+            <p><label for="chapter">Chapter: </label><input class="form-control" type="text" :value="currentActivist.chapter" id="chapter"></p>
+            <p><label for="phone">Phone: </label><input class="form-control" type="text" :value="currentActivist.phone" id="phone"></p>
+            <p><label for="location">Location: </label><input class="form-control" type="text" :value="currentActivist.location" id="location"></p>
+            <p><label for="facebook">Facebook: </label><input class="form-control" type="text" :value="currentActivist.facebook" id="facebook"></p>
+            <p><label for="core">Core/Staff:&nbsp;</label><input class="form-check-input" type="checkbox" id="core"></p>
+            <p><label for="exclude">Exclude from Leaderboard:&nbsp;</label><input class="form-check-input" type="checkbox" id="exclude"></p>
+            <p><label for="pledge">Liberation Pledge:&nbsp;</label><input class="form-check-input" type="checkbox" id="pledge"></p>
+            <p><label for="globalteam">Global Team Member:&nbsp;</label><input class="form-check-input" type="checkbox" id="globalteam"></p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click="hideModal">Close</button>
+            <button type="button" class="btn btn-success" @click="saveModal">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </modal>
+  </div>
+
+</template>
+
+<script>
+// Library from here: https://github.com/euvl/vue-js-modal
+import vmodal from 'vue-js-modal';
+import Vue from 'vue';
+
+Vue.use(vmodal);
+
+// Store the data of the previous sort.
+var previousSortData = {
+  field: null,
+  ascending: null,
+};
+
+export default {
+  name: 'activist-list',
+  methods: {
+    showModal: function(activist) {
+      this.currentActivist = activist;
+      this.$modal.show('edit-activist-modal');
+    },
+    hideModal: function() {
+      this.currentActivist = null;
+      this.$modal.hide('edit-activist-modal');
+    },
+    saveModal: function() {
+      console.log('not implemented');
+    },
+    modalOpened: function() {
+      // Add noscroll to body tag so it doesn't scroll while the modal
+      // is shown.
+      $(document.body).addClass('noscroll');
+    },
+    modalClosed: function() {
+      // Allow body to scroll after modal is closed.
+      $(document.body).removeClass('noscroll');
+    },
+    setActivists: function(activistsData) {
+      this.activists = activistsData;
+    },
+    sortBy: function(field) {
+      var ascending;
+      if (field == previousSortData.field) {
+        ascending = !previousSortData.ascending;
+      } else {
+        ascending = true;
+      }
+
+      this.activists.sort(function(a,b) {
+        if (ascending) {
+          return (a[field].toLowerCase() < b[field].toLowerCase()) ? -1 : 1;
+        } else {
+          return (a[field].toLowerCase() > b[field].toLowerCase()) ? -1 : 1;
+        }
+      });
+      previousSortData.field = field;
+      previousSortData.ascending = ascending;
+    },
+  },
+  data() {
+    return {
+      currentActivist: {},
+      activists: [],
+    };
+  },
+  created() {
+    $.ajax({
+      url: "/activist/list",
+      success: function(data) {
+        var parsed = JSON.parse(data);
+        if (parsed.status === "error") {
+          flashMessage("Error: " + parsed.message, true);
+          return;
+        }
+        // status === "success"
+
+        this.setActivists(parsed);
+      }.bind(this),
+      error: function() {
+        flashMessage("Error connecting to server.", true);
+      },
+    });
+  }
+}
+</script>
+
+<style>
+
+  .no-background-color {
+    background-color: transparent !important;
+  }
+
+  .noscroll {
+    overflow: hidden;
+  }
+
+  .v--modal-overlay {
+    overflow-y: scroll;
+    z-index: 1040 !important;
+  }
+
+</style>
--- frontend/activist_list.js
+++ frontend/activist_list.js
@@ -1,102 +1,11 @@
-import {flashMessage} from 'flash_message';
-
-var ACTIVISTS = [];
-
-var DAYS_AGO_60 = new Date();
-DAYS_AGO_60.setDate(DAYS_AGO_60.getDate() - 60);
-
-var DAYS_AGO_90 = new Date();
-DAYS_AGO_90.setDate(DAYS_AGO_90.getDate() - 90);
-
-function getActivistStatusHTML(activist, firstEventDate, lastEventDate) {
-  if (activist.name == "") {
-    return "";
-  }
-
-  if (!firstEventDate || !lastEventDate) {
-    return '<span class="hide">4</span>No attendance';
-  }
-
-  if (lastEventDate < DAYS_AGO_60) {
-    return '<span class="hide">3</span>Former';
-  } else if (firstEventDate > DAYS_AGO_90  && activist.total_events < 5) {
-    return '<span class="hide">2</span>New';
-  }
-
-  return '<span class="hide">1</span>Current';
-}
-
-function listActivists(activists) {
-  $("#activist-list-body").html('');
-  var d = document.getElementById('activist-list-body');
-  var m = document.getElementById('modals');
-
-  for (var i = 0; i < activists.length; i++) {
-    var activist = activists[i];
-
-    var firstEventDate = null;
-    var lastEventDate = null;
-
-    if (activist.first_event !== "") {
-      var firstEventSplit = activist.first_event.split('-');
-      firstEventDate = new Date(firstEventSplit[0],firstEventSplit[1]-1,firstEventSplit[2]);
-    }
-
-    if (activist.last_event !== "") {
-      var lastEventSplit = activist.last_event.split('-');
-      lastEventDate = new Date(lastEventSplit[0],lastEventSplit[1]-1,lastEventSplit[2]);
-    }
-
-    var activistStatus = getActivistStatusHTML(activist, firstEventDate, lastEventDate);
-
-    var modalID = 'modal' + activist.id;
-    var modal = '<div class="modal fade" id=' + modalID + ' tabindex="-1" role="dialog">' +
-        '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">' +
-        '<h2 class="modal-title">' + activist.name + '</h5></div>' +
-        '<div class="modal-body">' +
-        '<label for="email">Email: </label><input class="form-control" type="text" value="'+ activist.email + '" id="email"><br />' +
-        '<label for="chapter">Chapter: </label><input class="form-control" type="text" value="'+ activist.chapter + '" id="chapter"><br />' +
-        '<label for="phone">Phone: </label><input class="form-control" type="text" value="'+ activist.phone + '" id="phone"><br />' +
-        '<label for="location">Location: </label><input class="form-control" type="text" value="'+ activist.location + '" id="location"><br />' +
-        '<label for="facebook">Facebook: </label><input class="form-control" type="text" value="'+ activist.facebook + '" id="facebook"><br />' +
-        '<label for="core">Core/Staff:&nbsp;</label><input class="form-check-input" type="checkbox" id="core"><br />' +
-        '<label for="exclude">Exclude from Leaderboard:&nbsp;</label><input class="form-check-input" type="checkbox" id="exclude"><br />' +
-        '<label for="pledge">Liberation Pledge:&nbsp;</label><input class="form-check-input" type="checkbox" id="pledge"><br />' +
-        '<label for="globalteam">Global Team Member:&nbsp;</label><input class="form-check-input" type="checkbox" id="globalteam">' +
-        '<div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button>' +
-        '</div></div></div></div>'
-
-    var newRow = '<tr>' +
-        '<td>' + '<a class="edit-link" href="#" data-toggle="modal" data-target="#' + modalID + '"><button class="btn btn-default glyphicon glyphicon-pencil"></button></a>' + '</td>' +
-        '<td>' + activist.name + '</td>' +
-        '<td>' + activist.email + '</td>' +
-        '<td>' + activist.phone + '</td>' +
-        '<td>' + activist.first_event + '</td>' +
-        '<td>' + activist.last_event + '</td>' +
-        '<td>' + activistStatus + '</td>' +
-        '</tr>';
-    d.insertAdjacentHTML('beforeend', newRow);
-    m.insertAdjacentHTML('beforeend', modal);
-  }
-  $(function(){ $("#activist-list").tablesorter({ sortList: [[6,0], [1,0]] }); });
-}
+import ActivistList from 'ActivistList.vue';
+import Vue from 'vue';
 
 export function initializeApp() {
-  $.ajax({
-    url: "/activist/list",
-    success: function(data) {
-      var parsed = JSON.parse(data);
-      if (parsed.status === "error") {
-        flashMessage("Error: " + parsed.message, true);
-        return;
-      }
-      // status === "success"
-
-      ACTIVISTS = parsed;
-      listActivists(parsed);
-    },
-    error: function() {
-      flashMessage("Error connecting to server.", true);
-    },
+  var vm = new Vue({
+    el: "#app",
+    render: function(h) {
+      return h(ActivistList);
+    }
   });
 }
