6a5418f3af912899c92b2dc52e0d4a3456eda958
Samer Masterson <samer@samertm.com>
2017-08-06 20:59:22 -0700

    Make events list more responsive

--- frontend/event_list.js
+++ frontend/event_list.js
@@ -2,6 +2,22 @@ import {flashMessage} from 'flash_message';
 import 'bootstrap-chosen/bootstrap-chosen.css';
 import 'chosen-js'; // Attaches to jQuery when it's imported.
 
+// From chosen-js
+function chosenBrowserIsSupported() {
+  if ("Microsoft Internet Explorer" === window.navigator.appName) {
+    return document.documentMode >= 8;
+  }
+  if (/iP(od|hone)/i.test(window.navigator.userAgent) ||
+      /IEMobile/i.test(window.navigator.userAgent) ||
+      /Windows Phone/i.test(window.navigator.userAgent) ||
+      /BlackBerry/i.test(window.navigator.userAgent) ||
+      /BB10/i.test(window.navigator.userAgent) ||
+      /Android.*Mobile/i.test(window.navigator.userAgent)) {
+    return false;
+  }
+  return true;
+}
+
 export function confirmDeleteEvent(eventID) {
   var eventRow = $("#event-id-" + eventID);
   var eventName = eventRow.find(".event-name").text();
@@ -160,6 +176,15 @@ function initDateRange() {
 
 function initEventActivistSelect() {
   var $selector = $("#event-activist");
+
+  // Chosen-js isn't supported on mobile browsers. We need to add the
+  // class "form-control" to the selector if it isn't supported so the
+  // selector doesn't look super ugly.
+  if (!chosenBrowserIsSupported()) {
+    $selector.addClass('form-control');
+  }
+
+
   $.ajax({
     url: "/activist_names/get",
     method: "GET",
