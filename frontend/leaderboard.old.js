12fbb8d1948371ae4a219ca409a0307405868211
Jake Hobbs <jakehobbs@gmail.com>
2017-11-28 14:06:19 -0800

    new leaderboard using hot

---
+++ frontend/leaderboard.old.js
@@ -0,0 +1,37 @@
+import {flashMessage} from 'flash_message';
+
+function listActivists(activists) {
+  $("#activist-list-body").html('');
+  var d = document.getElementById('activist-list-body');
+  for (var i = 0; i < activists.length; i++) {
+    var activist = activists[i];
+    var newRow = '<tr>' +
+        '<td>' + (i + 1) + '.</td>' +
+        '<td>' + activist.name + '</td>' +
+        '<td>' + activist.points + '</td>' +
+        '<td>' + activist.total_events_30_days + '</td>' +
+        '<td>' + activist.total_events + '</td>' +
+        '<td>' + activist.level + '</td>' +
+        '</tr>';
+    d.insertAdjacentHTML('beforeend', newRow);
+  }
+}
+
+export function initializeApp() {
+  $.ajax({
+    url: "/leaderboard/list",
+    success: function(data) {
+      var parsed = JSON.parse(data);
+      if (parsed.status === "error") {
+        flashMessage("Error: " + parsed.message, true);
+        return;
+      }
+      // status === "success"
+
+      listActivists(parsed);
+    },
+    error: function() {
+      flashMessage("Error connecting to server.", true);
+    },
+  });
+}
