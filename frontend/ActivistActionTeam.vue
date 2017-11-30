f85ca851af9e286107c7c68a043680e74f2350f1
Jake Hobbs <jakehobbs@gmail.com>
2017-11-30 15:11:53 -0800

    focus fix?

--- frontend/ActivistActionTeam.vue
+++ frontend/ActivistActionTeam.vue
@@ -352,13 +352,13 @@ const allColumns = [
     data: "action_team_focus",
     colWidths: 160,
     type: 'dropdown',
-    source: [
-      "Direct Action",
-      "Community",
-      "Development",
-      "Finance",
-      "Communications",
-    ],
+    //source: [
+    //  "Direct Action",
+    //  "Community",
+    //  "Development",
+    //  "Finance",
+    //  "Communications",
+    //],
   },
   enabled: true,
 }, {
