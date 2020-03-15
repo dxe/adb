5303fc748be5db8bc4e4919a6b4e29b9e811e045
Liam Kirsh <liam.kirsh@riseup.net>
2020-03-15 02:16:56 -0700

    add CSRF protection to admin actions

--- frontend/UserList.vue
+++ frontend/UserList.vue
@@ -316,10 +316,12 @@ export default Vue.extend({
       // If the specified Role already exists in the Current User's role list,
       // then we assume the role should be removed.
       const existingRole = this.currentUser.roles.indexOf(role) >= 0;
+      const csrfToken = $('meta[name="csrf-token"]').attr("content")
 
       $.ajax({
         url: existingRole ? '/users-roles/remove' : '/users-roles/add',
         method: 'POST',
+        headers: { "X-CSRF-Token": csrfToken },
         contentType: 'application/json',
         data: JSON.stringify({
           user_id: this.currentUser.id,
