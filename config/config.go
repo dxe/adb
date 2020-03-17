ad8c42f162c08f167637c3e3a890e88f5802cf82
Samer Masterson <samer@samertm.com>
2020-03-17 13:17:35 -0700

    Make mailing list, surveys, and members page optional

--- config/config.go
+++ config/config.go
@@ -9,48 +9,48 @@ import (
 )
 
 var (
-	DBUser     = mustGetenv("DB_USER", "adb_user")
-	DBPassword = mustGetenv("DB_PASSWORD", "adbpassword")
-	DBName     = mustGetenv("DB_NAME", "adb_db")
-	DBProtocol = mustGetenv("DB_PROTOCOL", "")
+	DBUser     = mustGetenv("DB_USER", "adb_user", true)
+	DBPassword = mustGetenv("DB_PASSWORD", "adbpassword", true)
+	DBName     = mustGetenv("DB_NAME", "adb_db", true)
+	DBProtocol = mustGetenv("DB_PROTOCOL", "", true)
 
-	Port = mustGetenv("PORT", "8080")
+	Port = mustGetenv("PORT", "8080", true)
 
-	Route0 = mustGetenv("ROUTE_0", "/route0")
-	Route1 = mustGetenv("ROUTE_1", "/route1")
-	Route2 = mustGetenv("ROUTE_2", "/route2")
+	Route0 = mustGetenv("ROUTE_0", "/route0", true)
+	Route1 = mustGetenv("ROUTE_1", "/route1", true)
+	Route2 = mustGetenv("ROUTE_2", "/route2", true)
 
-	CookieSecret = mustGetenv("COOKIE_SECRET", "some-fake-secret")
+	CookieSecret = mustGetenv("COOKIE_SECRET", "some-fake-secret", true)
 
 	// Path to Google API oauth client_secrets.json file, with
 	// access to the following scope:
 	// https://www.googleapis.com/auth/admin.directory.group
 	// And the "Admin" API enabled. More info:
 	//   https://developers.google.com/api-client-library/python/auth/service-accounts
-	SyncMailingListsConfigFile = mustGetenv("SYNC_MAILING_LISTS_CONFIG_FILE", "")
+	SyncMailingListsConfigFile = mustGetenv("SYNC_MAILING_LISTS_CONFIG_FILE", "", false)
 
 	// The email for the user that that the oauth account should
 	// take action as.
-	SyncMailingListsOauthSubject = mustGetenv("SYNC_MAILING_LISTS_OAUTH_SUBJECT", "")
+	SyncMailingListsOauthSubject = mustGetenv("SYNC_MAILING_LISTS_OAUTH_SUBJECT", "", false)
 
 	// For sending surveys
-	AWSAccessKey       = mustGetenv("AWS_ACCESS_KEY_ID", "")
-	AWSSecretKey       = mustGetenv("AWS_SECRET_KEY", "")
-	AWSSESEndpoint     = mustGetenv("AWS_SES_ENDPOINT", "")
-	SurveyMissingEmail = mustGetenv("SURVEY_MISSING_EMAIL", "")
-	SurveyFromEmail    = mustGetenv("SURVEY_FROM_EMAIL", "")
+	AWSAccessKey       = mustGetenv("AWS_ACCESS_KEY_ID", "", false)
+	AWSSecretKey       = mustGetenv("AWS_SECRET_KEY", "", false)
+	AWSSESEndpoint     = mustGetenv("AWS_SES_ENDPOINT", "", false)
+	SurveyMissingEmail = mustGetenv("SURVEY_MISSING_EMAIL", "", false)
+	SurveyFromEmail    = mustGetenv("SURVEY_FROM_EMAIL", "", false)
 
 	// For members.dxesf.org
-	MembersClientID     = mustGetenv("MEMBERS_CLIENT_ID", "")
-	MembersClientSecret = mustGetenv("MEMBERS_CLIENT_SECRET", "")
+	MembersClientID     = mustGetenv("MEMBERS_CLIENT_ID", "", false)
+	MembersClientSecret = mustGetenv("MEMBERS_CLIENT_SECRET", "", false)
 )
 
-func mustGetenv(key, fallback string) string {
+func mustGetenv(key, fallback string, mandatory bool) string {
 	val := os.Getenv(key)
 	if val != "" {
 		return val
 	}
-	if !IsProd {
+	if !mandatory || !IsProd {
 		return fallback
 	}
 
