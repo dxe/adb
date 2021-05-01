e0d475e701f59a6982f9e9d49185813b43f2da06
Jake Hobbs <jakehobbs@gmail.com>
2021-04-30 22:40:08 -0700

    test emails

--- international_mailer/international_mailer.go
+++ international_mailer/international_mailer.go
@@ -95,6 +95,90 @@ Direct Action Everywhere Organizer</p>
 
 }
 
+func sendInternationalActionEmail(chapter model.ChapterWithToken) {
+	subject := "PLEASE READ - New database of DxE chapters and May Global Strategy Call"
+	body := `
+	<p>Hi all!</p>
+	
+	<p>Exciting news from our tech team! They have created an ADB (activist database) that will help automate some of the
+	International Coordination Working Group’s messaging and tasks and will automatically track international actions that
+	have facebook event pages if you are just able to make our tech team lead <a href="https://www.facebook.com/jhobbs91">Jake Hobbs</a> an admin of your chapter’s facebook
+	page. He won’t read your messages or interact with your page at all except to create and maintain an automated portal
+	that puts your upcoming event pages on <a href="http://dxe.io/events">"DxE’s website</a> so they can be found by visitors who are looking for events in
+	your area. If you haven’t already, please add <a href="https://www.facebook.com/jhobbs91">Jake</a> as an admin now.</p>
+	
+	<p>Also, with the new ADB, we’ll be better able to keep track of assuring that all chapters are organizing actions
+	(online or in person) each quarter in order to remain as an active chapter in the DxE International Organizers Network.
+	In place of the monthly report forms that we’ve used lately, your chapter will automatically receive this email on the
+	first and seventh of each month with <a href="">a link to a short form to report your previous last actions or ask for any assistance</a>.
+	Please keep an eye out for the email on the 1st of the month so we can be sure of any actions that you did the previous
+	month. Actions need not be an elaborate protest, especially during the pandemic, and can simply just be a social media
+	challenge or organizing your community members to email representatives or businesses with an ask.</p>
+	
+	<p>If you aren't able to do an action in a quarter, we will remove your chapter and invite you to return later if you
+	like or we can adjust your chapter’s status to “Hiatus” temporarily, but ideally, we’d just love for you to take some
+	form of action even if it’s online. Of course, In-person actions are great so if conditions permit, please consider
+	organizing a <a href="https://www.facebook.com/events/276596890763352"Let Dairy Die protest</a> on or around Mothers Day
+	this month or <a href="https://docs.google.com/spreadsheets/d/1-y_r8BgepiHpnOYyzoJISn30gNHuChUTBYxSUGWFSMg/edit?usp=sharing">list your address here</a> if you’d like to be
+	sent materials to do postering or a banner drop promoting Netflix’s Seaspiracy in your city. As always, also
+	please just consider organising whatever action works for your chapter’s goals. Thank you!</p>
+	
+	<p>We can discuss and share more during the <a href="https://www.facebook.com/events/126551249410206">May Global Strategy Call</a> tomorrow, Sunday, May 2nd at 10am Pacific.
+	We hope to see you there!</p>
+	
+	<p>Thank you for all that you do,</p>
+	
+	<p>Paul Darwin Picklesimer<br />
+	Direct Action Everywhere<br />
+	International Coordination Working Group<br />
+	(304) 479-3366</p>
+	`
+
+	var toEmails []string
+	if chapter.Email != "" {
+		toEmails = append(toEmails, chapter.Email)
+	}
+	if len(chapter.Organizers) > 0 {
+		for _, o := range chapter.Organizers {
+			if o.Email != "" {
+				toEmails = append(toEmails, o.Email)
+			}
+		}
+	}
+
+	// send to each person in the chapter
+	for _, e := range toEmails {
+		fmt.Printf("Should send email to %v\n", e)
+		//err := mailer.Send(mailer.Message{
+		//	FromName:    "Paul Darwin Picklesimer",
+		//	FromAddress: "paul@directactioneverywhere.com",
+		//	ToName:      "DxE " + chapter.Name,
+		//	ToAddress:   e,
+		//	Subject:     subject,
+		//	BodyHTML:    body,
+		//})
+		//if err != nil {
+		//	log.Println("Failed to send email for international form submission")
+		//}
+	}
+
+	// TODO: remove these test emails once you uncomment the above
+	if chapter.ChapterID == 20 || chapter.ChapterID == 47 || chapter.ChapterID == 48 {
+		err := mailer.Send(mailer.Message{
+			FromName:    "Paul Darwin Picklesimer",
+			FromAddress: "paul@directactioneverywhere.com",
+			ToName:      "DxE " + chapter.Name,
+			ToAddress:   "jake@dxe.io",
+			Subject:     subject,
+			BodyHTML:    body,
+		})
+		if err != nil {
+			log.Println("Failed to send email for international form submission")
+		}
+	}
+
+}
+
 func internationalMailerWrapper(db *sqlx.DB) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -123,7 +207,7 @@ func internationalMailerWrapper(db *sqlx.DB) {
 		// TODO: also check if the last email was sent last month
 		// TODO: also check if an email was sent this month but it's been over 8 days and no reply
 		if !chap.LastCheckinEmailSent.Valid {
-			fmt.Println("SHOULD SEND EMAIL TO", chap.ChapterID, chap.Name, chap.EmailToken)
+			sendInternationalActionEmail(chap)
 		}
 		// update LastCheckinEmailSent time
 		chap.LastCheckinEmailSent = sql.NullTime{
