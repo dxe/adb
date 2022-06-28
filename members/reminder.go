86462a3c89a94dac2a82bc13d6115f29621321f0
Matthew Dempsky <matthew@dempsky.org>
2022-06-27 18:41:35 -0700

    members: use Source Code Pro for <code> font

--- members/reminder.go
+++ members/reminder.go
@@ -118,10 +118,15 @@ var reminderTmpl = template.Must(template.New("reminder").Parse(`
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
 
 <style>
 body {
   font-family: 'Source Sans Pro', sans-serif;
+}
+
+code {
+  font-family: 'Source Code Pro', monospace;
 }
 
 .wrap {
