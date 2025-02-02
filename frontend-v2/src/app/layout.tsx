6bda74c1351c4d1085b19aa7f067db78402a2c1e
Alexander Taylor <ajxtaylor@gmail.com>
2025-02-02 03:33:42 +0000

    Add scaffolding for App Router with test page.
    React Query and NiceModal are not yet set up in the test page as they
    are in the Pages router version. Some work may be needed to get these
    working with the App router.
    
    Also remove IE header in Pages router and App router layouts.

---
+++ frontend-v2/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import '@/styles/globals.css'
+import type { Metadata, Viewport } from 'next'
+import { Toaster } from 'react-hot-toast'
+
+export const metadata: Metadata = {
+  title: 'Activist Database',
+  description: 'Activist Database for Direct Action Everywhere',
+  icons: '/v2/favicon.png',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+      </head>
+      <body className="antialiased">
+        {children}
+        <Toaster position="bottom-right" />
+      </body>
+    </html>
+  )
+}
