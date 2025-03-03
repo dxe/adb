a4a9ed08741aff6b7ae7e016e6fae642a0d9bf0e
jakehobbs <jakehobbs@gmail.com>
2025-03-02 17:47:52 -0800

    add react navbar

--- frontend-v2/src/app/layout.tsx
+++ frontend-v2/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from 'next'
 import { Toaster } from 'react-hot-toast'
 import Providers from './providers'
 import { cn } from '@/lib/utils'
+import '../../../frontend/static/external/buefy.min.css'
 
 export const metadata: Metadata = {
   title: 'Activist Database',
@@ -27,13 +28,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
       </head>
-      <body
-        className={cn(
-          'antialiased',
-          /* Prevent layout shift. See VueNavbar.tsx for details. */
-          'has-navbar-fixed-top',
-        )}
-      >
+      <body className={cn('antialiased', 'has-navbar-fixed-top')}>
         <Providers>{children}</Providers>
         <Toaster position="bottom-right" />
       </body>
