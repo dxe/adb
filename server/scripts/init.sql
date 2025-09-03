da4306e6f237c6a16da752320a74a520b5f39c84
Alexander Taylor <ajxtaylor@gmail.com>
2025-09-03 06:39:30 +0000

    fix: use same mysql charset/collation as prod
    Obtained charset/collation used in prod via:
    SELECT * FROM information_schema.SCHEMATA  WHERE schema_name = "adb2";
    
    This change allows saving emojis to the database in dev which the app does
    when creating a chapter: the flag is set by the frontend automatically
    to an emoji corresponding to the the country.
    
    Since the database is created during devcontainer creation / docker compose
    init, and since production is already set to the correct value, this
    isn't fixed by creating a database migration file as we usually do.
    
    To fix your development environment set up before this commit, either:
    
    1. Remove database and rebuild container
    
    $ docker container ls
    $ docker container stop dxe-adb-mysql-1
    $ docker container rm dxe-adb-mysql-1
    $ docker volume ls
    $ docker volume rm dxe-adb_mysql-data
    
    Then use VS Code command palette "Dev Containers: Rebuild Container"
    and re-run `make deps` and `make dev_db`
    
    2. OR, drop and recreate tables manually
    
    > DROP DATABASE adb_db;
    > DROP DATABASE adb_test_db;
    
    Then refer to init.sql for CREATE DATABASE commands.
    Finally, re-run `make dev_db`.
    
    Note that ALTER DATABASE to change the default may not update all
    the table/column settings.

--- server/scripts/init.sql
+++ server/scripts/init.sql
@@ -2,5 +2,5 @@ CREATE USER 'adb_user'@'%' IDENTIFIED BY 'adbpassword';
 GRANT ALL PRIVILEGES ON *.* to 'adb_user'@'%';
 FLUSH PRIVILEGES;
 
-CREATE DATABASE adb_db CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE adb_test_db CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE adb_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE adb_test_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
