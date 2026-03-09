cca200ab2b5e68293473e11c90fab556cd9dba01
jakehobbs <jakehobbs@gmail.com>
2026-03-08 20:43:32 -0700

    fix: aria

--- frontend-v2/src/components/ui/sort-indicator.tsx
+++ frontend-v2/src/components/ui/sort-indicator.tsx
@@ -3,7 +3,12 @@ import clsx from 'clsx'
 export function SortIndicator({ sorted }: { sorted: false | 'asc' | 'desc' }) {
   return (
     <span className={clsx('text-xs', { invisible: !sorted })}>
-      {sorted === 'desc' ? '▼' : '▲'}
+      <span aria-hidden="true">{sorted === 'desc' ? '▼' : '▲'}</span>
+      {sorted && (
+        <span className="sr-only">
+          {sorted === 'desc' ? 'sorted descending' : 'sorted ascending'}
+        </span>
+      )}
     </span>
   )
 }
